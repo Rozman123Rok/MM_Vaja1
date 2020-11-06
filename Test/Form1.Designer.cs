3258aefe60692cad8738cd762cd171e4002663d6
Rok Rozman <rok.rozman3@gmail.com>
2020-11-06 18:22:59 +0100

    Koncano

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(274, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(669, 588);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(669, 588);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nalozi BP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Nalozi CT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Prikazi sliko";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 506);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 44);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Kompresiraj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 315);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(43, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vpisi T:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(47, 556);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 44);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Dekompresiraj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 614);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using iTextSharp.text.pdf.codec;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+
+        int izbrali_BP = 0, izbrali_CT = 0; // ce so ze vnesli
+        byte[,] barve = new byte[256, 3]; // BP
+        short[,] slika = new short[513, 513];  // slika ki si jo bom shranil
+        Bitmap moja_slika = new Bitmap(513, 513); // da ustvarim sliko
+        int T = 0; // T vrednost kot koliko je lahko napake
+        BinaryReader br; // za branje datotek
+        int img_size = 512; // velikost slike
+
+
+        String string1 = ""; // to so deli niza
+        String string2 = "";
+        String string3 = "";
+        String string4 = "";
+
+
+        int Zacetek = 0, Konec = 512; // zacetek pa konec pri kompresiji
+        string niz = ""; // niz ki ga izpisem
+
+
+        int bits = 12; // koliko bitov rabim
+        short vrednost = 0;
+        int index;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // nalozi BP
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+
+                br = new BinaryReader(File.Open(dialog.FileName, FileMode.Open));
+
+                for (int i = 0; i < 256; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        barve[i, j] = br.ReadByte();
+                    }
+                }
+            }
+
+            izbrali_BP = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // NALOZI CT
+            OpenFileDialog dialog = new OpenFileDialog(); // odprem fileDialog da lahko izberem datoteko
+
+            if (dialog.ShowDialog() == DialogResult.OK) // ce je ok rezultat
+            {
+                br = new BinaryReader(File.Open(dialog.FileName, FileMode.Open)); // odprem datoteko
+                for (int i = 0; i < img_size; i++)
+                {
+                    for (int j = 0; j < img_size; j++)
+                    {
+                        slika[i, j] = br.ReadInt16(); // si jo shranim v sliko
+                    }
+                }
+            }
+
+            izbrali_CT = 1; // da sem nalozil sliko
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // PRIKAZI SLIKO
+            if (izbrali_BP == 1 && izbrali_CT == 1)
+            {
+                for (int i = 0; i < 512; i++)
+                {
+                    for (int j = 0; j < 512; j++)
+                    {
+                        double temp = ((slika[i, j] + 2048.0) / 4095.0) * 255.0;
+                        int a = slika[i, j];
+
+                        moja_slika.SetPixel(i, j, Color.FromArgb(barve[(int)temp, 0], barve[(int)temp, 1], barve[(int)temp, 2]));
+                    }
+                }
+
+                pictureBox1.Image = moja_slika;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // KOMPRESIRAJ
+            T = int.Parse(textBox1.Text); // pridobim T
+            int polovica = Konec / 2; // kje je polovica izracunam
+            //System.Windows.MessageBox.Show("asd" + polovica);
+
+            // si pripravim 4 nize ki so samo deli niza
+            string1 = "";
+            string2 = "";
+            string3 = "";
+            string4 = "";
+
+            // naredim 4 taske da grejo razdelit sliko
+            Task n1 = Task.Factory.StartNew(() => razdeliSliko(Zacetek, Zacetek, polovica, 1)); // levo zgoraj (0,0)
+            Task n2 = Task.Factory.StartNew(() => razdeliSliko(Zacetek + polovica, Zacetek, polovica, 2)); // desno zgoraj (polovica,0) 
+            Task n3 = Task.Factory.StartNew(() => razdeliSliko(Zacetek, Zacetek + polovica, polovica, 3)); // levo spodaj (0, polovica)
+            Task n4 = Task.Factory.StartNew(() => razdeliSliko(Zacetek + polovica, Zacetek + polovica, polovica, 4)); // desno spodaj (polovica, polovica)
+
+            //pocakam da se vsi taski zaklucijo
+            Task.WaitAll(n1, n2, n3, n4);
+
+            niz += string1 + string2 + string3 + string4; // zdruzim stringe v en niz
+            niz = Convert.ToString(9, 2).PadLeft(8, '0') + niz;
+
+            
+            Zapisi_v_Dat(); // zapisem v dat
+            System.Windows.MessageBox.Show("KONCANO");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string text = File.ReadAllText(@"out.txt");
+            niz = text; // shranim prebrano v niz
+            dek();
+            System.Windows.MessageBox.Show("KONCANO");
+            izbrali_CT = 1; // ker ko je dekompresije konec je isto koda sem si nalozil sliko
+
+        }
+        private void razdeliSliko(int s1, int s2, int velikost, int id)
+        {// s1 in s2 sta zacetni koordinati , velikost je koliko jih je
+
+            int polovica; // koliko je polovica
+            bool lahkoKomp = true; // ce so vsi enake vrednosti
+            short prvi = slika[s1, s2]; // vzamem prvi element ki ga bom primerjal
+            for (int i = s1; i < s1 + velikost; i++)
+            {
+                for (int j = s2; j < s2 + velikost; j++)
+                {
+                    if (Math.Abs(slika[i, j] - prvi) > T)
+                    {
+                        lahkoKomp = false; // se ne ujema moremo razdelit
+                    }
+                }
+            }
+
+            if (lahkoKomp)
+            {
+                // potem lahko zapisujem v niz
+                if (prvi < 0)
+                {
+                    // ima negativno vrednost
+                    /// -2048 bit je 1000 0000 0000 in more 0 bit spredaj
+                    switch (id)
+                    {
+                        // pogledam keri id ma thread
+                        case 1:
+                            string1 += "01" + Convert.ToString((short)(prvi * -1), 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 2:
+                            string2 += "01" + Convert.ToString((short)(prvi * -1), 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 3:
+                            string3 += "01" + Convert.ToString((short)(prvi * -1), 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 4:
+                            string4 += "01" + Convert.ToString((short)(prvi * -1), 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                    }
+                }
+                else
+                {
+                    //2047 = 0111 1110 1011 in 0 spredaj
+                    switch (id)
+                    {
+                        case 1:
+                            string1 += "00" + Convert.ToString(prvi, 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 2:
+                            string2 += "00" + Convert.ToString(prvi, 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 3:
+                            string3 += "00" + Convert.ToString(prvi, 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                        case 4:
+                            string4 += "00" + Convert.ToString(prvi, 2).PadLeft(12, '0').Substring(1, 11);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                // morem se naprej delit zapisem 1
+                // in v string si shranim 1
+                switch (id)
+                {
+                    case 1:
+                        string1 += "1";
+                        break;
+                    case 2:
+                        string2 += "1";
+                        break;
+                    case 3:
+                        string3 += "1";
+                        break;
+                    case 4:
+                        string4 += "1";
+                        break;
+                }
+
+                // ponovno vse 4 delitve
+                polovica = (velikost / 2);
+                razdeliSliko(s1, s2, polovica, id);
+                polovica = (velikost / 2);
+                razdeliSliko(s1 + polovica, s2, polovica, id);
+                polovica = (velikost / 2);
+                razdeliSliko(s1, s2 + polovica, polovica, id);
+                polovica = (velikost / 2);
+                razdeliSliko(s1 + polovica, s2 + polovica, polovica, id);
+            }
+
+        }
+
+        private void Zapisi_v_Dat()
+        {
+            // zapisem v datoteko
+            File.WriteAllText(@"out.txt", niz);
+        }
+
+        private void dek() {
+            int n = Convert.ToInt32(niz.Substring(index, 8), 2);
+            //Console.WriteLine("n: " + n);
+
+            Zacetek = 0;
+            Konec = (int)Math.Pow(2, n); // kako velika je slika 
+            int polovica = Konec / 2; // jo damo na polovico
+
+            // rekurzivno delim sliko na 4 dele
+            slika = new short[Konec, Konec]; // naredim novo polje za sliko
+            index = 8;
+            delitevDek(Zacetek, Zacetek, polovica);
+            delitevDek(Zacetek + polovica, Zacetek, polovica);
+            delitevDek(Zacetek, Zacetek + polovica, polovica);
+            delitevDek(Zacetek + polovica, Zacetek + polovica, polovica);
+        }
+
+        private void delitevDek(int s1, int s2, int velikost) {
+            if (niz[index] == '1')
+            {
+                // ce je 1 smo naprej delili
+                int pol = (velikost / 2);
+                index++;
+                delitevDek(s1, s2, pol);
+                delitevDek(s1 + pol, s2, pol);
+                delitevDek(s1, s2 + pol, pol);
+                delitevDek(s1 + pol, s2 + pol, pol);
+            }
+            else
+            {
+                // 0 torej imamo eno barvo
+                //preberem 12 bitov in zapišem to barvo not 
+                index++;
+                if (niz[index] == '1')
+                    vrednost = -1;
+                else
+                    vrednost = 1;
+
+                index++;
+
+                if ((short)Convert.ToInt32(niz.Substring(index, bits - 1), 2) == 0 && vrednost == -1)
+                    vrednost = -2048;
+                else
+                    vrednost *= (short)Convert.ToInt32(niz.Substring(index, 11), 2);
+
+
+
+                for (int i = s1; i < s1 + velikost; i++)
+                {
+                    for (int j = s2; j < s2 + velikost; j++)
+                    {
+                        slika[i, j] = vrednost; // si shranimo v sliko
+                    }
+                }
+
+                index += (bits - 1);
+            }
+        }
+
+    }
+}
